d870330c7229e0c3d7fffb88a15483cd3360d6d4
Adam B <abdev@Adams-MacBook-Pro.local>
2019-08-25 15:40:03 -0400

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="layout">
+    <header class="header">
+      <strong>
+        <g-link class="logo" to="/">{{ $static.metaData.siteName }}</g-link>
+      </strong>
+      <nav class="nav">
+        <g-link class="nav__link" to="/">Home</g-link>
+        <g-link class="nav__link" to="/about">About</g-link>
+      </nav>
+    </header>
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss">
+.logo {
+  font-family: "Libre Barcode 39 Extended Text", cursive;
+  text-transform: uppercase;
+  font-size: 4rem;
+  color: $mine-shaft;
+  font-weight: 400;
+
+  &:visited,
+  &:hover {
+    color: $mine-shaft;
+  }
+}
+
+.layout {
+  max-width: 140rem;
+  margin: 0 auto;
+  padding-left: 20px;
+  padding-right: 20px;
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  height: 80px;
+}
+
+.nav__link {
+  margin-left: 20px;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,53 @@
+<template>
+  <Layout>
+    <div class="page">
+      <div class="left-side-panel">
+        <h1>Home</h1>
+      </div>
+
+      <div class="content">
+        <p
+          class="intro"
+        >I build internet. I am an Atlanta based Full Stack Developer with a focus in Javascript, Vue.js &amp; JAM stack.</p>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Home"
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+  position: relative;
+}
+
+.left-side-panel {
+  position: absolute;
+  top: 10rem;
+  left: 3rem;
+  transform: rotate(-90deg);
+}
+
+.left-side-panel h1 {
+  font-size: 1.4rem;
+  text-transform: uppercase;
+  color: $blue;
+}
+
+.content {
+  max-width: 80%;
+  margin: 0 auto;
+}
+
+.content .intro {
+  font-size: 8.2rem;
+  margin: 0;
+  text-decoration: underline;
+}
+</style>
